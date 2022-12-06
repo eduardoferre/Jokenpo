697355b7675a30815adf508e4b607d021028e3ec
almei <almei@EduardoFerreira>
2022-12-06 16:30:56 -0300

    commit

--- Jokenpo/src/Jokenpo/Jokenpo.java
+++ Jokenpo/src/Jokenpo/Jokenpo.java
@@ -1,9 +1,16 @@
 package Jokenpo;
 
+import java.util.Scanner;
+
 public class Jokenpo {
 
 	public static void main(String[] args) {
 		System.out.print("Hello Worlds!");
+		
+		Scanner scanf = new Scanner(System.in);
+		Integer n1;
+		n1 = scanf.nextInt();
+		System.out.print(n1);
 
 	}
 
